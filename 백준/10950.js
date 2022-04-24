76c455edc17492357127a50f5abe4481453afe16
Sejun Park <qkrtpwns1999@naver.com>
2022-04-25 03:41:12 +0900

    백준 :10950 해결 그냥 for문만 되는 이유는 모르겟음

--- 백준/10950.js
+++ 백준/10950.js
@@ -5,11 +5,24 @@ const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString();
 function solution(input) {
   const [T, ...inputs] = input.split("\n");
-  inputs.forEach((element) => {
-    const [first, second] = element.split(" ");
+
+  // inputs.forEach((element) => {
+  //   const [first, second] = element.split(" ");
+  //   const answer = parseInt(first, 10) + parseInt(second, 10);
+  //   console.log(answer);
+  // });
+
+  // for (element of inputs) {
+  //   const [first, second] = element.split(" ");
+  //   const answer = parseInt(first, 10) + parseInt(second, 10);
+  //   console.log(answer);
+  // }
+
+  for (let i = 0; i < T; i += 1) {
+    const [first, second] = inputs[i].split(" ");
     const answer = parseInt(first, 10) + parseInt(second, 10);
     console.log(answer);
-  });
+  }
 }
 
 solution(input);

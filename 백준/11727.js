775ecb67037321e004dc5197e9b52969bf9d47d4
박세준 <tpwns6693@gmail.com>
2022-04-28 18:17:00 +0900

    입출력 완료로 인한 아카이빙, DP 문제 해결 1463 9095 10844 11726 11727

---
+++ 백준/11727.js
@@ -0,0 +1,20 @@
+let cache = []; //배열크기  N + 1로
+function recursive(N) {
+  //기저
+  if (N === 1) return 1;
+  if (N === 2) return 3;
+  if (cache[N] != null) {
+    return cache[N];
+  }
+  cache[N] = (recursive(N - 1) + 2 * recursive(N - 2)) % 10007;
+  return cache[N];
+}
+function solution(input) {
+  const N = Number(input);
+  const cacheSize = N + 1;
+  cache = new Array(cacheSize).fill(null);
+  result = recursive(N);
+  console.log(result);
+}
+const input = require("fs").readFileSync("./input.txt").toString();
+solution(input);

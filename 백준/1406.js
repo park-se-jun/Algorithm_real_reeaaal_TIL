4ef384bca1ee98e34a736be82ed3f26ae63d8729
박세준 <tpwns6693@gmail.com>
2022-05-19 00:22:52 +0900

    백준 1406:링크드 리스트로 구현 하다가 잘못됨 다시 해야함

--- 백준/1406.js
+++ 백준/1406.js
@@ -1,54 +1,100 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+    addNext(value){
+        let node = new Node(value);
+        if(this.next!=null){
+            let nextnext = this.next;
+            this.next = node;
+            this.next.prev = this;
+            this.next.next =nextnext;
+            this.next.next.prev = this.next;
+        }else{
+            this.next = node;
+            this.next.prev = this;
+        }
 
-class Editor{
-    constructor(string){
-        this.stringArray = string.split("");
-        this.cursur = this.stringArray.length;
+        return this.next;
     }
-    moveLeft(){
-        if(this.cursur===0) return;
-        this.cursur -= 1;
-    }
-    moveRight(){
-        if(this.cursur === this.stringArray.length) return;
-        this.cursur += 1;
-    }
-    removeBefore(){
-        if(this.cursur === 0) return;
-        this.cursur -= 1;
-        this.stringArray.splice(this.cursur,1)
-    }
-    addAfter(value){//P
-        this.stringArray.splice(this.cursur,0,value);
-        this.cursur += 1;
+    addPrev(value){
+        let node = new Node(value);
+        if(this.prev!=null){
+            let prevprev = this.prev;
+            this.prev = node;
+            this.prev.next = this;
+            this.prev.prev = prevprev;
+            this.prev.prev.next = this.prev;
+        }else{
+            this.prev = node;
+            this.prev.next = this;
+        }
 
+        return this.prev;
     }
-    getString(){
-        return this.stringArray.join("");
+    getPrev(){//L
+        if(this.prev === null) return this;
+        return this.prev;
+    }
+    getNext(){//D
+        if(this.next === null) return this;
+        return this.next;
+    }
+    deletePrev(){//B
+        let prevprev = this.prev.getPrev();
+        this.prev= prevprev;
+        this.prev.next = this;
+        return this;
+    }
+    P(value){//P
+        this.addPrev(value);
+        return this;
+    }
+    printAll(){
+        let cursur = this;
+        let result = "";
+        while(cursur.getPrev()===cursur){
+            cursur=cursur.getPrev();
+        }
+        while(cursur.getNext()===cursur){
+            result += cursur.value;
+            cursur = cursur.getNext();
+        }
+        return result;
     }
 }
 function solution(input){
     let[value,N,...remain]= input.split("\n")
-    let myEditor = new Editor(value.trim());
+    let cursur;
+    for(let element of value.trim().split("")){
+        if(cursur===undefined){
+            cursur = new Node(element);
+        }else{
+            cursur = cursur.addNext(element);
+        }
+    }
     for(element of remain){
         let [method, char] = element.split(" ").map(x=>x.trim());
         switch(method){
             case "L":
-                myEditor.moveLeft();
+                cursur = cursur.getPrev();
                 break;
             case "D":
-                myEditor.moveRight();
+                cursur = cursur.getNext();
                 break;
             case "B":
-                myEditor.removeBefore();
+                cursur = cursur.deletePrev();
                 break;
             case "P":
-                myEditor.addAfter(char);
+                cursur = cursur.P(char);
                 break;
             default:
                 break;
         }
     }
-    console.log(myEditor.getString())
+    console.log(cursur.printAll());
 }
 let path = process.platform === "linux"?"/dev/stdin":"./input.txt";
 let input = require("fs").readFileSync(path).toString().trim();

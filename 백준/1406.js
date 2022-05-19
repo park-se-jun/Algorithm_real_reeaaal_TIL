27768825ea2c416fdc0609c4e2a241787f652679
SeJun Park <qkrtpwns1999@naver.com>
2022-05-19 17:09:26 +0900

    백준 1406:stack 2개로 풀이 나중에 링크드리스트로 다시 풀것

--- 백준/1406.js
+++ 백준/1406.js
@@ -1,76 +1,46 @@
-class LinkedListNode{
-    constructor(data){
-        this.prev = null;
-        this.next = null;
-    }
-}
-class DoubleLinkedList{
-    constructor(){
-        this.head = null;
-        this.tail = null;
-        this.cursur = null;
-        this.size = 0;
-    }
-    isEmpty(){
-        return this.size === 0;
-    }
-    moveCursurFront(){
-        this.cursur = this.cursur.prev;
-    }
-    moveCursurBack(){
-        this.cursur = this.cursur.next;
-    }
-    addCursurFront(value){
 
-    }
-    addCursurBack(value){
-
-    }
-    deleteCursur(){
-        let tmpNode =this.cursur;
-        try{
-            tmpNode.prev.next = tmpNode.next;
-            tmpNode.next.prev = tmpNode.prev;    
-        } catch(e){
-            if(tmpNode.prev===null){//가장 앞이면
-
-            }else if(tmpNode.next === null){// 가장 뒤면
-                
-
-            }
-        }
-    }
-}
 function solution(input){
+    let beforeCursur = [];
+    let afterCursur = [];
+    let tmpChar;
     let[value,N,...remain]= input.split("\n")
-    let cursur;
     for(let element of value.trim().split("")){
-        if(cursur===undefined){
-            cursur = new Node(element);
-        }else{
-            cursur = cursur.addNext(element);
-        }
+        beforeCursur.push(element);
     }
     for(element of remain){
         let [method, char] = element.split(" ").map(x=>x.trim());
         switch(method){
             case "L":
-                cursur = cursur.getPrev();
+                tmpChar = beforeCursur.pop();
+                if(tmpChar === undefined){
+                    break;
+                }
+                afterCursur.push(tmpChar);
+                tmpChar = null;
                 break;
             case "D":
-                cursur = cursur.getNext();
+                tmpChar = afterCursur.pop();
+                if(tmpChar === undefined){
+                    break;
+                }
+                beforeCursur.push(tmpChar);
+                tmpChar = null;
                 break;
             case "B":
-                cursur = cursur.deletePrev();
+                beforeCursur.pop();
                 break;
             case "P":
-                cursur = cursur.P(char);
+                beforeCursur.push(char);
                 break;
             default:
                 break;
         }
     }
-    console.log(cursur.printAll());
+    while(afterCursur.length!=0){
+        tmpChar=afterCursur.pop();
+        beforeCursur.push(tmpChar);
+    }
+    console.log(beforeCursur.join(""));
 }
 let path = process.platform === "linux"?"/dev/stdin":"./input.txt";
 let input = require("fs").readFileSync(path).toString().trim();

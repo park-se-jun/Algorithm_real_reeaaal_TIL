c49f004279c9ae8978813f8e5d305cd5b184d222
박세준 <tpwns6693@gmail.com>
2022-12-20 05:11:43 +0900

    프로그래머스LV3 : 코딩테스트 공부 dp 효율성 실패

---
+++ 프로그래머스/lv3/alpAndCop.js
@@ -0,0 +1,48 @@
+/**
+ * 
+ * @param {Number} alp 
+ * @param {Number} cop 
+ * @param {Array<Array<Number>>} problems 
+ * @returns {Number}
+ */
+function solution(initAlp, initCop, problems) {
+    const alpReqArray = problems.map(element => element[0]);
+    const copReqArray = problems.map(element => element[1]);
+    const maxAlp = Math.max(...alpReqArray);
+    const maxCop = Math.max(...copReqArray);
+    console.log(maxAlp,maxCop);
+    const cache = Array.from({length:151},()=>Array(151).fill(undefined));
+    var answer = 0;
+    return dp(maxAlp,maxCop);
+    
+        
+    function dp(targetAlp,targetCop){
+
+        if(targetAlp<=initAlp && targetCop<=initCop){
+            cache[targetAlp][targetCop] = 0;
+        } 
+        if(cache[targetAlp][targetCop]!=undefined){
+            return cache[targetAlp][targetCop]
+        }
+        //할 수 있는 것
+        //직전에 알고리즘 공부를 할 경우
+        const studyalgo = targetAlp>initAlp?dp(targetAlp-1,targetCop)+1:Infinity;
+        //직전에 코딩 공부를 할 경우
+        const studyCoding = targetCop>initCop?dp(targetAlp,targetCop-1) + 1:Infinity;
+        //직전에 문제를 풀었을 경우
+        let solveProblem = Infinity;
+        for (const element of problems) {
+            //못푸는 문제(알고력이 딸리거나 나 코딩력이 딸림)는 고려하지 않아도 됨
+            if(targetAlp-element[2]<element[0] || targetCop-element[3] <element[1] ){
+                continue;
+            }else{
+                solveProblem = Math.min(solveProblem,dp(targetAlp-element[2],targetCop-element[3])+element[4]);
+            }
+        }
+        cache[targetAlp][targetCop] = Math.min(solveProblem,studyCoding,studyalgo);
+        return cache[targetAlp][targetCop];
+    }
+}
+
+solution(0, 0, [[0, 0, 2, 1, 2], [4, 5, 3, 1, 2], [4, 11, 4, 0, 2], [10, 4, 0, 4, 2]])
+console.log(cache)
